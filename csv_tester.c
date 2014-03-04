2b678b148323fa38063e17014f89be94d6575a27
bioster <brent.oster@gmail.com>
2014-03-04 17:09:46 -0500

    Create csv_tester.c
    Placeholder

---
+++ csv_tester.c
@@ -0,0 +1,47 @@
+// A placeholder for the tests
+
+#include <stdio.h>
+
+
+/*
+
+test_string = "field1,field2,field3\r\n" +
+
+"\"aaa\r\n\",\"bb,b\",\"ccc\"\r\n" +
+
+"\"in \"\"quotes\"\"\",2,3\r\n" +
+
+"1,2,\r\n" +
+
+"zzz,yyy,xxx\r\n" +
+
+"1,,3\r\n" +
+
+",,";
+
+
+
+expected_output = new string[][]{
+
+    new string[]{"field1", "field2", "field3"},
+
+    new string[]{ "aaa\r\n", "bb,b", "ccc"},
+
+    new string[]{"in \"quotes\"", "2", "3"},
+
+    new string[]{"1", "2", ""},
+
+    new string[]{"zzz", "yyy", "xxx"},
+
+    new string[]{"1", "", "3"},
+
+    new string[]{"", "", ""}};
+
+*/
+
+int main(int argc, char **argv)
+{
+	// Return a numer.  Any number is fine.
+	printf("Placeholder.");
+	return 0;
+}
