8580b4d3828e2bd9e56fe6c718fb3cf944b9d167
bioster <brent.oster@gmail.com>
2014-03-05 11:11:50 -0500

    Update libcsv_parser.c
    Started writing the parser.
    Decided to just iterate over the input directly, since it's not that complex.
    Altered the fake test input to add a slash in the middle of the word 'field3'.

--- libcsv_parser.c
+++ libcsv_parser.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
 // Parse a line, return when we're done.
 // Update loc whenever we move forward a character.
 void parse_line(char input[], int* loc)
@@ -13,32 +14,68 @@ void parse_line(char input[], int* loc)
 	printf("loc is %d\n", *loc);
 	puts(input);
 
-	// Is this character only 1-character wide?
-	// Escapes and line endings are multiple characters.
-	
-	// Is this character a comma?
-	
-	// Is this character at the start of a word?
-	// Is it at the start of a word, and is a quote?
+
 	
 	return;
 }
-
+*/
 
 extern void parse_csv()
 {
 	printf("Reached parser\n");
 	
-	// Fake the input:
-	char input[] = "field1,field2,field3\r\n\"aaa\r\n\",\"bb,b\",\"ccc\"\r\n\"in \"\"quotes\"\"\",2,3\r\n1,2,\r\nzzz,yyy,xxx\r\n1,,3\r\n,,";
+	// Fake some input:
+	char input[] = "field1,field2,fi\\\\eld3\r\n\"aaa\r\n\",\"bb,b\",\"ccc\"\r\n\"in \"\"quotes\"\"\",2,3\r\n1,2,\r\nzzz,yyy,xxx\r\n1,,3\r\n,,";
 	
 	// Keep track of where we are in the parse
 	int location = 0;
+	// Track if we are currently inside quotes
+	char in_quotes = 0;
 	
-	// Iterate over the input, breaking it up into lines.
-	// Every time we've just ended a line either we're done or we start another.
+	/*
 	parse_line(input, &location);
 	location = 1;
 	parse_line(&input, &location);
+	*/
+	
+	while (input[location] != NULL)
+	{
+		// Iterate over the input
+		
+		switch(input[location])
+		{
+			case '"':
+				// Handle quotes.
+				if (in_quotes)
+				{
+					// We were already in quotes, so end the quote.
+					in_quotes = 0;
+					printf("-EQ-",input[location]);// TEST output
+				} else {
+					// We were not inside quotes, so start a new quoted section.
+					in_quotes = 1;
+					printf("-SQ-",input[location]); // TEST output
+				}
+				break;
+			case '\\':
+				// The next character is escaped.  Handle that here.
+				// Escaped characters cannot be special.
+				location++;
+				printf("%c",input[location]);// TEST output
+				break;
+			case ',':
+				if (in_quotes)
+				{
+					printf("%c",input[location]);// TEST output
+				} else {
+					// Start a new cell
+					// TODO
+				}
+			// TODO: line endings
+			default:
+				printf("%c",input[location]);// TEST output
+		}
+		location++;
+	}
 		
 }
