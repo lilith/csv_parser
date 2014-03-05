f6292b2d88c4c764bd9e1d4f603d20257d4c90a8
bioster <brent.oster@gmail.com>
2014-03-04 19:19:09 -0500

    Update cs_csv_wrapper.cs

--- cs_csv_wrapper.cs
+++ cs_csv_wrapper.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Runtime.InteropServices;
 
-// A plcaeholder for the .NET wrapper
- 
-public class cs_helloworld
+// A wrapper for the CSV parser
+
+namespace my_csv_wrapper
+{
+public class cs_csv_wrapper
 {
 	[DllImport("libcsv_parser.so")]
-	public static extern int parse_csv();
+	public static extern void parse_csv();
 
-	static public void Main (string[] args)
+	public static void do_parse_csv()
 	{
-		Console.WriteLine ("Placeholder");
+		Console.WriteLine ("Reached wrapper");
+		parse_csv();
 	}
+	
+}
 }
