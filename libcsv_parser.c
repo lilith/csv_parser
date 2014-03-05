d861d3308fc492de6d5d8498cabc8d154c7d55c2
bioster <brent.oster@gmail.com>
2014-03-04 19:18:34 -0500

    Update libcsv_parser.c

--- libcsv_parser.c
+++ libcsv_parser.c
@@ -5,11 +5,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern void parse_csv()
+
+// Parse a line, return when we're done.
+// Update loc whenever we move forward a character.
+void parse_line(char input[], int* loc)
 {
-	// First we're going to iterate over the input and output to the console.
-	//printf("%s", input);
+	printf("Reached parse_line\n");
+	printf("loc is %d\n", *loc);
+	puts(input);
+
+	// Is this character only 1-character wide?
+	// Escapes and line endings are multiple characters.
 	
-	printf("Reached parser\n");
+	// Is this character a comma?
+	
+	// Is this character at the start of a word?
+	// Is it at the start of a word, and is a quote?
+	
+	return;
 }
 
+
+extern void parse_csv()
+{
+	printf("Reached parser\n");
+	
+	// Fake the input:
+	char input[] = "field1,field2,field3\r\n\"aaa\r\n\",\"bb,b\",\"ccc\"\r\n\"in \"\"quotes\"\"\",2,3\r\n1,2,\r\nzzz,yyy,xxx\r\n1,,3\r\n,,";
+	
+	// Keep track of where we are in the parse
+	int location = 0;
+	
+	// Iterate over the input, breaking it up into lines.
+	// Every time we've just ended a line either we're done or we start another.
+	parse_line(input, &location);
+	location = 1;
+	parse_line(&input, &location);
+		
+}
