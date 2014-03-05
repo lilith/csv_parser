eeb98b8b0444b91e6b8b3124acda4872aa3acb5e
bioster <brent.oster@gmail.com>
2014-03-05 15:08:20 -0500

    Update cs_csv_wrapper.cs
    Passing in the filename to the parser now.

--- cs_csv_wrapper.cs
+++ cs_csv_wrapper.cs
@@ -8,12 +8,12 @@ namespace my_csv_wrapper
 public class cs_csv_wrapper
 {
 	[DllImport("libcsv_parser.so")]
-	public static extern void parse_csv();
+	public static extern void parse_csv(string filename);
 
-	public static void do_parse_csv()
+	public static void do_parse_csv(string filename)
 	{
 		Console.WriteLine ("Reached wrapper");
-		parse_csv();
+		parse_csv(filename);
 	}
 	
 }
