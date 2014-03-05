2fb6b04344a4d5757c5ae52222b8f4024a62cdf6
bioster <brent.oster@gmail.com>
2014-03-05 15:08:59 -0500

    Update cs_csv_tester.cs
    Passing in a filename to the parser now.

--- cs_csv_tester.cs
+++ cs_csv_tester.cs
@@ -3,16 +3,13 @@ using System.Runtime.InteropServices;
 using my_csv_wrapper;
 
 // A csv test
-
- 
 public class cs_csv_tester
 {
-	[DllImport("libcsv_parser.so")]
-	public static extern int parse_csv();
+
 
 	static public void Main (string[] args)
 	{
-		string test_string = "field1,field2,field3\r\n" +
+/*		string test_string = "field1,field2,field3\r\n" +
 
 			"\"aaa\r\n\",\"bb,b\",\"ccc\"\r\n" +
 
@@ -43,10 +40,10 @@ public class cs_csv_tester
 			new string[]{"1", "", "3"},
 
 			new string[]{"", "", ""}};
-	
+*/	
 		Console.WriteLine ("Starting CSV Tester");
 		
-		cs_csv_wrapper.do_parse_csv();
+		cs_csv_wrapper.do_parse_csv("testfile.csv");
 	}
 }
 
