d8d98cec2c2ee4548fb6c7ca1d0ead742364d9e5
bioster <brent.oster@gmail.com>
2014-03-05 11:49:18 -0500

    Update libcsv_parser.c
    Gonna take a break for lunch.
    I think this is covering all the basic cases.  Note things we COULD handle without difficulty but aren't because they're out of spec include:
    Handling different separators
    allowing CL or RF to end lines, not just CLRF
    
    On a side note: Really?  Escaping characters with \ isn't part of the spec?  I think I want to leave that in... it's too useful.

--- libcsv_parser.c
+++ libcsv_parser.c
@@ -5,25 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-// Parse a line, return when we're done.
-// Update loc whenever we move forward a character.
-void parse_line(char input[], int* loc)
-{
-	printf("Reached parse_line\n");
-	printf("loc is %d\n", *loc);
-	puts(input);
-
-
-	
-	return;
-}
-*/
-
+// TODO: Take filename of file to be parsed
 extern void parse_csv()
 {
 	printf("Reached parser\n");
-	
+
+	// Read file to be parsed
 	// Fake some input:
 	char input[] = "field1,field2,fi\\\\eld3\r\n\"aaa\r\n\",\"bb,b\",\"ccc\"\r\n\"in \"\"quotes\"\"\",2,3\r\n1,2,\r\nzzz,yyy,xxx\r\n1,,3\r\n,,";
 	
@@ -31,49 +18,61 @@ extern void parse_csv()
 	int location = 0;
 	// Track if we are currently inside quotes
 	char in_quotes = 0;
+	const int separator = (int)','; // Don't set this to something wierd like a backslash.
 	
-	/*
-	parse_line(input, &location);
-	location = 1;
-	parse_line(&input, &location);
-	*/
-	
+	// Iterate over the input
 	while (input[location] != NULL)
 	{
-		// Iterate over the input
 		
-		switch(input[location])
+		if (input[location] == separator)
 		{
-			case '"':
-				// Handle quotes.
-				if (in_quotes)
-				{
-					// We were already in quotes, so end the quote.
-					in_quotes = 0;
-					printf("-EQ-",input[location]);// TEST output
-				} else {
-					// We were not inside quotes, so start a new quoted section.
-					in_quotes = 1;
-					printf("-SQ-",input[location]); // TEST output
-				}
-				break;
-			case '\\':
-				// The next character is escaped.  Handle that here.
-				// Escaped characters cannot be special.
-				location++;
+			// Out of spec option that could be added here: Handle other separators
+			if (in_quotes)
+			{
 				printf("%c",input[location]);// TEST output
-				break;
-			case ',':
-				if (in_quotes)
-				{
+			} else {
+				// Start a new cell
+				printf(">"); // TEST output
+			}
+		} else {
+			switch(input[location])
+			{
+				case '"':
+					// Handle quotes.
+					if (in_quotes)
+					{
+						// We were already in quotes, so end the quote.
+						in_quotes = 0;
+						printf("-EQ-");// TEST output
+					} else {
+						// We were not inside quotes, so start a new quoted section.
+						in_quotes = 1;
+						printf("-SQ-"); // TEST output
+					}
+					break;
+				case '\\':
+					// The next character is escaped.  Handle that here.
+					// Escaped characters cannot be special.
+					location++;
 					printf("%c",input[location]);// TEST output
-				} else {
-					// Start a new cell
-					// TODO
-				}
-			// TODO: line endings
-			default:
-				printf("%c",input[location]);// TEST output
+					break;
+				case '\r':
+					if (in_quotes)
+					{
+						printf("%c",input[location]);// TEST output
+					} else {
+						// This looks like the start of a line ending.
+						if (input[location] == '\n')
+						{
+							printf("CLRF\r\n",input[location]);// TEST output
+						} else {
+							printf("%c",input[location]);// TEST output
+						}
+					}
+				// Out of spec option that could be added here: allow CL or RF individually to end lines
+				default:
+					printf("%c",input[location]);// TEST output
+			}
 		}
 		location++;
 	}
